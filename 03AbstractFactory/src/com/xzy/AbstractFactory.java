bd3719ff29972003b58a2eef007cc51218725241
xzy <2894702202@qq.com>
2024-05-22 16:09:08 +0800

    feat:图片及补充

--- 03AbstractFactory/src/com/xzy/AbstractFactory.java
+++ 03AbstractFactory/src/com/xzy/AbstractFactory.java
@@ -2,6 +2,14 @@ package com.xzy;
 
 /**
  * 抽象工厂模式
+ *
+ * 意图：提供一个创建一系列相关或相互依赖对象的接口，而无须指定它们具体的类。
+ *
+ * 适用性：
+ * 1. 一个系统要独立于它的产品的创建、组合和表示时。
+ * 2. 一个系统要由多个产品系列中的一个来配置时。
+ * 3. 当要强调一系列相关的产品对象的设计以便进行联合使用时。
+ * 4. 当提供一个产品类库，只想显示它们的接口而不是实现时。
  */
 public class AbstractFactory {
     public static void main(String[] args) {
@@ -16,7 +24,6 @@ public class AbstractFactory {
         productA2.info();
         ProductB productB2 = factory2.createProductB();
         productB2.info();
-
     }
 }
 
