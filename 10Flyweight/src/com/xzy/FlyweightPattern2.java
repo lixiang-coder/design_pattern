25afe5a8f9ccfaba72057ade721c44721d28c8b6
xzy <2894702202@qq.com>
2024-05-23 11:52:26 +0800

    feat:新增Flyweight模式图片及补充

--- 10Flyweight/src/com/xzy/FlyweightPattern2.java
+++ 10Flyweight/src/com/xzy/FlyweightPattern2.java
@@ -4,6 +4,19 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
+/**
+ * 享元模式
+ * <p>
+ * 意图：运用共享技术有效地支持大量细粒度的对象。
+ * <p>
+ * 适用性：
+ * 1. 一个应用程序使用了大量的对象。
+ * 2. 完全由于使用大量的对象，造成很大的存储开销。
+ * 3. 对象的大多数状态都可变为外部状态。
+ * 4. 如果删除对象的外部状态，那么可以用相对较少的共享对象取代很多组对象。
+ * 5. 应用程序不依赖于对象标识。由于Flyweight对象可以被共享，所以对于概念上明显有别的对象，标识测试将返回真值。
+ */
+
 public class FlyweightPattern2 {
     public static void main(String[] args) {
         ShapeFactory factory = new ShapeFactory();
