19be0506171ce3f1a5e6b3db53473a313771f337
xzy <2894702202@qq.com>
2024-05-24 21:22:17 +0800

    feat:新增Proxy模式图片及补充

---
+++ 19Proxy/src/com/xzy/image/com/xzy/ProxyPattern.java
@@ -0,0 +1,47 @@
+package com.xzy.image.com.xzy;
+
+/**
+ * 代理模式
+ * <p>
+ * 意图：为其他对象提供一种代理以控制对这个对象的访问。
+ * <p>
+ * 适用性：
+ * Proxy模式适用于在需要比较通用和复杂的对象指针代替简单的指针的时候
+ */
+public class ProxyPattern {
+    public static void main(String[] args) {
+        RealSubject realSubject = new RealSubject();
+        Proxy proxy = new Proxy(realSubject);
+
+        proxy.buy();
+    }
+}
+
+
+interface Subject {
+    void buy();
+}
+
+
+class Proxy implements Subject {
+    protected RealSubject realSubject;
+
+    public Proxy(RealSubject realSubject) {
+        this.realSubject = realSubject;
+    }
+
+    @Override
+    public void buy() {
+        System.out.println("办理购买前的手续~");
+        realSubject.buy(); // 付钱
+        System.out.println("办理购买后的手续~");
+    }
+}
+
+
+class RealSubject implements Subject {
+    @Override
+    public void buy() {
+        System.out.println("付钱~");
+    }
+}
